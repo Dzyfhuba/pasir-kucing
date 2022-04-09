5fc447caa090c35b78b74fbc4917c4919ea700e1
Dzyfhuba <hafidz21ub@gmail.com>
2022-04-10 00:04:12 +0700

    finnaly, i found it.

---
+++ resources/views/home/head.blade.php
@@ -0,0 +1,15 @@
+<article id="head">
+    <div class="container-md">
+        <div id="content">
+            <h1 id="title">Kucing Pasir</h1>
+            <p id="description">
+                Kucing pasir adalah kucing yang dapat menjadi tempat tinggal bagi kucing yang
+                membutuhkan kamar yang nyaman. Kucing pasir dapat menjadi tempat tinggal
+                bagi kucing yang membutuhkan kamar yang nyaman. Kucing pasir dapat menjadi
+                tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman. Kucing pasir
+                dapat menjadi tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman.
+            </p>
+            <button class="">kenali kami lebih baik</button>
+        </div>
+    </div>
+</article>
