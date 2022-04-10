540509fe51d7845594495f17c19e57793b369f8d
Dzyfhuba <hafidz21ub@gmail.com>
2022-04-10 22:01:44 +0700

    add readme

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,12 +1,14 @@
 <article id="about">
-    <h1 class="title">
-        Pasir Kucing
-    </h1>
-    <p class="description">
-        Pasir kucing adalah kucing yang dapat menjadi tempat tinggal bagi kucing yang
-        membutuhkan kamar yang nyaman. Pasir kucing dapat menjadi tempat tinggal
-        bagi kucing yang membutuhkan kamar yang nyaman. Pasir kucing dapat menjadi
-        tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman. Pasir kucing
-        dapat menjadi tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman.
-    </p>
+    <div class="wrapper">
+        <h1 class="title">
+            Pasir Kucing
+        </h1>
+        <p class="description">
+            Pasir kucing adalah kucing yang dapat menjadi tempat tinggal bagi kucing yang
+            membutuhkan kamar yang nyaman. Pasir kucing dapat menjadi tempat tinggal
+            bagi kucing yang membutuhkan kamar yang nyaman. Pasir kucing dapat menjadi
+            tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman. Pasir kucing
+            dapat menjadi tempat tinggal bagi kucing yang membutuhkan kamar yang nyaman.
+        </p>
+    </div>
 </article>
